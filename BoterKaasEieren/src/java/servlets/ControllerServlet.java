7c8a3fabb6607eddfc7082323184f3a9425da875
erwin-engel <erwinengle@gmail.com>
2016-12-12 08:06:08 +0100

    show score added, first version

--- BoterKaasEieren/src/java/servlets/ControllerServlet.java
+++ BoterKaasEieren/src/java/servlets/ControllerServlet.java
@@ -3,62 +3,75 @@ package servlets;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.*;
 
-
 /**
- * Verantwoordelijkheden van ControllerServlet: � Inlezen userList bij starten
- * server � Wegschrijven userLst bij aflsuiten server � userList toevoegen aan
- * elke HTTP-request � HTTP-requests doorgeven.
+ * Verantwoordelijkheden van ControllerServlet: - nieuw Spel object aanmaken bij
+ * eerste aanroep - keuzes van gebruiker na starten spel doorgeven aan Spel
+ * object - Spel object toevoegen aan elke HTTP-request - HTTP-request
  */
 public class ControllerServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-
     private BoterKaasEierenSpel boKaEiSpel = null;
-    private Status status = null;
 
     /**
      * Uitvoeren bij het starten van de servlet (als server wordt gestart).
      */
     public void init() throws ServletException {
-        // Niet nodig ?
+        // deze methode wordt nog niet gebruikt
     }
 
     /**
-     * Uitvoeren bij het starten van de servlet (als server wordt gestart) 
+     * Uitvoeren bij het starten van de servlet (als server wordt gestart)
      */
     public void destroy() {
-        // Niet nodig ?
+        // deze methode wordt nog niet gebruikt
     }
+
     /**
-     * Keuze 'nieuw spel' komt terecht bij de ControllerServlet via een HTTP
-     * get-request.
+     * De web.xml welcome-file-list zorgt ervoor dat de eerste aanroep door
+     * gebruiker bij de ControllerServlet terecht komt via een HTTP get-request
+     *
+     * Keuze 'nieuw spel' na eerste aanroep komt terecht bij de
+     * ControllerServlet via een HTTP get-request.
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse 
+            response) throws ServletException, IOException {
         System.out.println("do get");
-        String url = "";
+        HttpSession session = getSession(request); 
+        boKaEiSpel = new BoterKaasEierenSpel(Markering.KRUIS);
+        String url = "/Display_BoKaEiSpel.jsp";
         String bron = request.getParameter("bron");
-        System.out.println(bron);
-        HttpSession session = getSession(request);
+        // als de parameter "bron" niet aanwezig is dan is de bron van de 
+        // aanroep de web.xml file. (welcome-file-list: aller eerste aanroep 
+        // door user)
+        if (bron == null) {
+            bron = "webXmlBo-Ka-Ei";
+        }
         switch (bron) {
+            case "webXmlBo-Ka-Ei":
+                boKaEiSpel.setBedenkSlim(true);
+                session.setAttribute("boKaEiSpel", boKaEiSpel);
+                // Haal de scoreCookies op en zet ze in het sessie object
+                Cookie[] scoreCookies = getScoreCookies(request);
+                for (int i = 0; i < scoreCookies.length; i++) {
+                    session.setAttribute(scoreCookies[i].getName(), 
+                            scoreCookies[i]);
+                }
+                break;
             case "bo_ka_ei":
-                System.out.println("bo_ka_ei get");
-                boKaEiSpel = new BoterKaasEierenSpel(Markering.KRUIS);
-                session.setAttribute("spelStatus", boKaEiSpel.getStatus());
-                if (request.getParameter("slim").equals("true")){
+                if (request.getParameter("slim").equals("true")) {
                     boKaEiSpel.setBedenkSlim(true);
                 } else {
                     boKaEiSpel.setBedenkSlim(false);
                 }
                 session.setAttribute("boKaEiSpel", boKaEiSpel);
-                session.setAttribute("spelStatus", boKaEiSpel.getStatus());
-                url = "/Display_BoKaEiSpel.jsp";
                 break;
             default:
                 url = "/unknown_bron.jsp";
@@ -70,23 +83,21 @@ public class ControllerServlet extends HttpServlet {
     }
 
     /**
-     * doPost wordt verwerkt als de gebruiker een formulier heeft aangeboden.
+     * doPost wordt verwerkt als de gebruiker een zet heeft gedaan (er wordt dan
+     * een formulier aangeboden)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+             HttpServletResponse response)throws ServletException, IOException {
         System.out.println("do post");
-        String url = "";
-        String bron = request.getParameter("bron");
-        int spelerZetIndex = Integer.parseInt(request.getParameter("spelerZetIndex"));
         HttpSession session = getSession(request);
-
+        int spelerZetIndex = Integer.parseInt(request.getParameter("spelerZetIndex"));
+        String url = "/Display_BoKaEiSpel.jsp";
+        String bron = request.getParameter("bron");
         switch (bron) {
             case "bo_ka_ei":
-                System.out.println("bo_ka_ei post spelerZetIndex = " + spelerZetIndex);
-                boKaEiSpel.verwerkZet(Markering.KRUIS, spelerZetIndex);        
+                boKaEiSpel.verwerkZet(Markering.KRUIS, spelerZetIndex);
                 boKaEiSpel.verwerkZet(Markering.NUL, boKaEiSpel.bedenkZet(Markering.NUL));
                 session.setAttribute("boKaEiSpel", boKaEiSpel);
-                session.setAttribute("spelStatus", boKaEiSpel.getStatus());
-                url = "/Display_BoKaEiSpel.jsp";
                 break;
             default:
                 url = "/unknown_bron.jsp";
@@ -97,13 +108,46 @@ public class ControllerServlet extends HttpServlet {
         dispatcher.forward(request, response);
     }
 
+    /**
+     * Deze methode wordt door doGet en doPost methode gebruikt om het sessie
+     * object op te halen. Als de sessie nog geen spel object heeft (i.g.v. 
+     * eerste aanroep) dan wordt spel aan sessie toegevoegd
+     */
     private HttpSession getSession(HttpServletRequest request) {
-        HttpSession session = request.getSession();  // maak sesion object
+        HttpSession session = request.getSession();
         if (session.getAttribute("boKaEiSpel") == null) {
-            System.out.println("maak spel instance");
             session.setAttribute("boKaEiSpel", boKaEiSpel);
         }
         return session;
     }
+    
+    /**
+     * Haal de score cookies op uit het request. Als deze er niet zijn maak dan 
+     * nieuwe scoreCookies
+     */
+    private Cookie[] getScoreCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String userScoreCookieName = "userScoreCookie";
+        String userScoreCookieValue = "";
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                System.out.println("cookies not null, length > nul");
+                Cookie cookie = cookies[i];
+                if (userScoreCookieName.equals(cookie.getName())) {
+                    System.out.println("aanwezig");
+                    userScoreCookieValue = cookie.getValue();
+                // deze volgende situatie doet zich allen voor als 
+                // het spel voor de eerste keer is getoond en de speler
+                //  gebruik de refresh button:
+                } else {
+                    userScoreCookieValue = "0";
+                }
+            }
+        } else {
+            userScoreCookieValue = "0";
+        }
+        Cookie[] cookiesNew = new Cookie[1];
+        cookiesNew[0] = new Cookie(userScoreCookieName, userScoreCookieValue);
+        return cookiesNew;
+    }
 }
-
--- BoterKaasEieren/web/Display_BoKaEiSpel.jsp
+++ BoterKaasEieren/web/Display_BoKaEiSpel.jsp
@@ -1,6 +1,8 @@
 <%@ page import="model.*" %>
 <%
     BoterKaasEierenSpel boKaEiSpel = (BoterKaasEierenSpel) session.getAttribute("boKaEiSpel");
+    Cookie scoreCookie = (Cookie) session.getAttribute("userScoreCookie");
+    int score = Integer.parseInt(scoreCookie.getValue());
 %>
 <html>
     <head>
@@ -14,67 +16,81 @@
             <h1>Webapplicaties: de server-kant</h1>
             <h1>Boter, Kaas en Eieren</h1>
         </div>
-        
-<% if (boKaEiSpel != null) { %>
         <div id="speelbord">
             <table cellspacing="5" cellpadding="5" border="1">
-                    <% for (int i = 0; i < 3; i++) { %>
-                    <tr>
-                        <%  int indexUp = 0;
-                            if (i == 1) {
-                               indexUp = 2; }
-                            if (i == 2) {
-                               indexUp = 4; }
-                            for (int j = 0; j < 3 ; j++) { %>
-                            <% if (boKaEiSpel.getMarkering( i + j + indexUp ) == Markering.KRUIS) { %>
-                                <td><label>X</label></td>
-                            <% } else if (boKaEiSpel.getMarkering( i +  j + indexUp ) == Markering.NUL) {  %>
-                                <td><label>O</label></td>
-                            <% } else {  %>
-                                <td>
-                                    <form action="<%= response.encodeURL("ControllerServlet")%>" method="post">
-                                        <input type="hidden" name="bron" value="bo_ka_ei">
-                                        <input type="hidden" name="spelerZetIndex" value="<%= i +  j + indexUp %>">                     
-                                        <input type="submit" value="kies">
-                                    </form>
-                                </td>
-                                <% } %>
-                        <% } %>
-                    </tr>
-                   <% } %>
+                <% for (int i = 0; i < 3; i++) { %>
+                <tr>
+                    <%  int indexUp = 0;
+                        if (i == 1) {
+                            indexUp = 2;
+                        }
+                        if (i == 2) {
+                            indexUp = 4;
+                        }
+                        for (int j = 0; j < 3; j++) {
+                            if (boKaEiSpel.getMarkering(i + j + 
+                                indexUp) == Markering.KRUIS) { %>
+                    <td><label>X</label></td>
+                    <% } else if (boKaEiSpel.getMarkering(i + j + 
+                            indexUp) == Markering.NUL) {  %>
+                    <td><label>O</label></td>
+                    <% } else {%>
+                    <td>
+                        <form action="<%= response.encodeURL("ControllerServlet")%>" method="post">
+                            <input type="hidden" name="bron" value="bo_ka_ei">
+                            <input type="hidden" name="spelerZetIndex" value="<%= i + j + indexUp%>">                     
+                            <input type="submit" 
+                                   <% if (boKaEiSpel.getStatus() == Status.BEZIG) { %>
+                                   value="kies">
+                            <% } else { %>
+                            value="">
+                            <% } %>
+                        </form>
+                    </td>
+                    <% } %>
+                    <% } %>
+                </tr>
+                <% } %>
             </table>
         </div>
-<% }%>
+        <div id="controls">
+            <p>score = <%= score%> </p>       
+        </div>
         <div id="controls">
             <form action="<%= response.encodeURL("ControllerServlet")%>" method="get"> 
                 Selecteer een speelwijze:<br>
                 <select name="slim">
-                    <option value="true" selected>Computer speelt slim
-                    <option value="false" >Computer speelt dom
+                    <option value="true" 
+                            <% if (boKaEiSpel.isBedenkSlim()) { %> 
+                            selected="selected" 
+                            <% } %>
+                            >Computer speelt slim
+                    <option value="false"
+                            <% if (!boKaEiSpel.isBedenkSlim()) { %>
+                            selected="selected"
+                            <% } %>
+                            >Computer speelt dom
                 </select><br>
                 <input type="hidden" name="bron" value="bo_ka_ei"><br>
                 <p><input type="submit" value="Nieuw spel"></p>
             </form>
         </div>
-
         <div id="status">
-            <% if ((Status)session.getAttribute("spelStatus") == Status.BEZIG){ %>
-                <p>zet een kruis!</p>
+            <% if (boKaEiSpel.getStatus() == Status.BEZIG) { %>
+            <p>doe een zet </p>
             <% }%>
-            <% if ((Status)session.getAttribute("spelStatus") == Status.KRUISWINT){ %>
-                <p>je hebt gewonnen!</p>
+            <% if (boKaEiSpel.getStatus() == Status.KRUISWINT) { %>
+            <p>je hebt gewonnen!</p>
             <% }%>
-            <% if ((Status)session.getAttribute("spelStatus") == Status.NULWINT){ %>
-                <p>Kaas wint :-(</p>
+            <% if (boKaEiSpel.getStatus() == Status.NULWINT) { %>
+            <p>Kaas wint :-(</p>
             <% }%>
-            <% if ((Status)session.getAttribute("spelStatus") == Status.GELIJK){ %>
-                <p>Gelijk spel...</p>
+            <% if (boKaEiSpel.getStatus() == Status.GELIJK) { %>
+            <p>Gelijk spel...</p>
             <% }%>
         </div>
-
         <div id="voettekst">
             <p>&copy; Copyright Open Universiteit 2009</p>
         </div>
     </body>
-
 </html>
